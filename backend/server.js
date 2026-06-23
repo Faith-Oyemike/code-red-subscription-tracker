39f68c1d387af9a930a0aa818859f1f1d0a1ad1b
Faith-Oyemike <faithoyemike12@gmail.com>
2026-06-23 08:34:26 +0100

    initial project setup and first express server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+//Go and find the Express package that I installed with npm and give it to me.
+const express = require("express"); //Store whatever Express returns inside a variable called express
+
+const app = express(); //create an express application and store in the variable called "app"
+
+const PORT = 3000; //The Port number the server will use to listen for incoming requests (its like a door number)
+
+// When someone visits the homepage ("/"),
+//Listen for a GET request on the route or endpoint ("/").
+// send this response back
+app.get("/", (req, res) => {
+  res.send("Welcome to Code Red Subscription Tracker!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
