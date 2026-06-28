1439f86b8a79030437e3a6a3df335a4af4a575e2
Faith-Oyemike <faithoyemike12@gmail.com>
2026-06-28 17:20:05 +0100

    organised subscription routes using express router

---
+++ backend/routes/subscriptionRoutes.js
@@ -0,0 +1,27 @@
+//import express package
+const express = require("express");
+
+//create a router to organise subcription routes
+const route = express.Router();
+
+//listen for a get request on the "/subscription" route
+route.get("/subscription", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Netflix",
+      price: 10.0,
+      renewalDate: "2026-07-01",
+    },
+
+    {
+      id: 2,
+      name: "AppleMusic",
+      price: 15.0,
+      renewalDate: "2026-07-05",
+    },
+  ]);
+});
+
+//export route so server.js can see it
+module.exports = Router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 //Go and find the Express package that I installed with npm and give it to me.
 const express = require("express"); //Store whatever Express returns inside a variable called express
 
+//import subscription route from the route folder
+const subscriptionRoute = require("./route/subscriptionRoutes");
 const app = express(); //create an express application and store in the variable called "app"
 
 const PORT = 3000; //The Port number the server will use to listen for incoming requests (its like a door number)
@@ -10,24 +12,6 @@ const PORT = 3000; //The Port number the server will use to listen for incoming 
 // send this response back
 app.get("/", (req, res) => {
   res.send("Welcome to Code Red Subscription Tracker!");
-});
-
-app.get("/subscription", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: "Netflix",
-      price: 10.0,
-      renewalDate: "2026-07-01",
-    },
-
-    {
-      id: 2,
-      name: "AppleMusic",
-      price: 15.0,
-      renewalDate: "2026-07-05",
-    },
-  ]);
 });
 
 app.listen(PORT, () => {
